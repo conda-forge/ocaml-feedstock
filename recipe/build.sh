cae4593fe98da19527a42d66916b1c091420c1e2
peterjc <p.j.a.cock@googlemail.com>
2018-04-13 14:58:35 +0100

    Tweak file command debugging

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,8 @@ make install
 echo $PREFIX
 head -n 1 $PREFIX/bin/ocamldebug
 head -n 1 $PREFIX/bin/ocamldoc
-head -n 1 $PREFIX/bin/ocamldoc.opt
+# This file should be binary
+# head -n 1 $PREFIX/bin/ocamldoc.opt
 file $PREFIX/bin/ocamldebug
 file $PREFIX/bin/ocamldoc
 file $PREFIX/bin/ocamldoc.opt
