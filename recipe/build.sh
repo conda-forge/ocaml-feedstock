8cf23fbf08fbf79688592a27750d3f998fdfe7d3
Isuru Fernando <isuruf@gmail.com>
2019-02-13 21:20:15 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export CC=$(basename "$CC")
+if [[ "$(uname)" == "Linux" ]]; then
+   export HASHBANGSCRIPTS=false
+else
+   export HASHBANGSCRIPTS=false
+fi
 bash -x ./configure -prefix $PREFIX -cc $CC -aspp "$CC -c" -as "$AS"
-make world.opt -j${CPU_COUNT} HASHBANGSCRIPTS=false
-make tests HASHBANGSCRIPTS=false
-make install HASHBANGSCRIPTS=false
+make world.opt -j${CPU_COUNT} HASHBANGSCRIPTS=${HASHBANGSCRIPTS}
+make tests HASHBANGSCRIPTS=${HASHBANGSCRIPTS}
+make install HASHBANGSCRIPTS=${HASHBANGSCRIPTS}
