38e4f2ef50a81365df2634283f4854b7310d9b0b
Isuru Fernando <isuruf@gmail.com>
2019-02-14 00:48:15 -0600

    Use a patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export CC=$(basename "$CC")
-HASHBANGSCRIPTS=false
 bash -x ./configure -prefix $PREFIX -cc $CC -aspp "$CC -c" -as "$AS"
-make world.opt -j${CPU_COUNT} HASHBANGSCRIPTS=${HASHBANGSCRIPTS}
-make tests HASHBANGSCRIPTS=${HASHBANGSCRIPTS}
-make install HASHBANGSCRIPTS=${HASHBANGSCRIPTS}
+make world.opt
+make tests
+make install
