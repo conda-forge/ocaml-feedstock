18c19b9e65ff236cc46f4687c6d8097f63a97610
Isuru Fernando <isuruf@gmail.com>
2019-02-13 19:57:31 -0600

    No  HASHBANGSCRIPTS
    Conda env path might be longer than 80 chars

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CC=$(basename "$CC")
 bash -x ./configure -prefix $PREFIX -cc $CC -aspp "$CC -c" -as "$AS"
-make world.opt -j${CPU_COUNT}
-make tests
-make install
+make world.opt -j${CPU_COUNT} HASHBANGSCRIPTS=false
+make tests HASHBANGSCRIPTS=false
+make install HASHBANGSCRIPTS=false
