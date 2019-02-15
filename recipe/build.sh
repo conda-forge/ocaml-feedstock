a160d17c52a026646e46c1a3d6c923767b8b6e44
Isuru Fernando <isuruf@gmail.com>
2019-02-15 14:20:07 -0600

    Add tests again

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 export CC=$(basename "$CC")
 bash -x ./configure -prefix $PREFIX -cc $CC -aspp "$CC -c" -as "$AS"
 make world.opt -j${CPU_COUNT}
+make tests
 make install
 
 for CHANGE in "activate" "deactivate"
