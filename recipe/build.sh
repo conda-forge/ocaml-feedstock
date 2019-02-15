14619c637b55ffd7abb285ed52e7a79c274908b8
Isuru Fernando <isuruf@gmail.com>
2019-02-15 14:17:09 -0600

    Don't replace prefixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export CC=$(basename "$CC")
 bash -x ./configure -prefix $PREFIX -cc $CC -aspp "$CC -c" -as "$AS"
-make world.opt
-make tests
+make world.opt -j${CPU_COUNT}
 make install
+
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
