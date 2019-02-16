dbafa4f4a22a870d6ed2ad6f33910d6bf3981be3
Isuru Fernando <isuruf@gmail.com>
2019-02-16 15:28:44 -0600

    Fix flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,10 @@ export AS=$(basename "$AS")
 export OCAML_PREFIX=$PREFIX
 export OCAMLLIB=$PREFIX/lib/ocaml
 
+sed -i.bak "s@common_cflags=\"\-O@common_cflags=\"${CFLAGS} \-O@g" configure
+sed -i.bak "s@common_cppflags=\"\"@commond_cppflags=\"${CPPFLAGS}\"@g" configure
+sed -i.bak "s@ldflags=\"\"@ldflags=\"${LDFLAGS}\"@g" configure
+
 bash -x ./configure -prefix $OCAML_PREFIX -cc $CC -aspp "$CC -c" -as "$AS"
 make world.opt -j${CPU_COUNT}
 make tests
