cb2a8c25ba6b1c0def31421c9bb828444d0fd50c
mementorc <claude.rc@gmail.com>
2025-08-20 15:29:03 -0500

    (ref) add non-unix

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,14 +6,22 @@ export ASPP="$CC -c"
 export AS=$(basename "$AS")
 export AR=$(basename "$AR")
 export RANLIB=$(basename "$RANLIB")
-export OCAML_PREFIX=$PREFIX
-export OCAMLLIB=$PREFIX/lib/ocaml
+
+if [[ "${target_platform}" != "linux-"* ]] && [[ "${target_platform}" != "osx-"* ]]; then
+  export OCAML_PREFIX=$PREFIX/Library
+  SH_EXT="bat"
+else
+  export OCAML_PREFIX=$PREFIX
+  SH_EXT="sh"
+fi
+
+export OCAMLLIB=$OCAML_PREFIX/lib/ocaml
 
 CONFIG_ARGS=(
   --enable-ocamltest
   --enable-shared
   --disable-static
-  --mandir=${PREFIX}/share/man
+  --mandir=${OCAML_PREFIX}/share/man
   --with-target-bindir=/opt/anaconda1anaconda2anaconda3/bin
   -prefix $OCAML_PREFIX
 )
@@ -23,39 +31,39 @@ CONFIG_ARGS=(
 #  fi
 #fi
 
-bash -x ./configure "${CONFIG_ARGS[@]}"
+bash ./configure "${CONFIG_ARGS[@]}"
 
 make world.opt -j${CPU_COUNT}
 
 # Check if cross-compiling - not testing on build architecture
-#if [[ ${CONDA_BUILD_CROSS_COMPILATION:-"0"} == "0" ]]; then
-#  if [ "$(uname)" = "Darwin" ]; then
-#    # Tests failing on macOS. Seems to be a known issue.
-#    rm testsuite/tests/lib-str/t01.ml
-#    rm testsuite/tests/lib-threads/beat.ml
-#  fi
-#
-#  if [[ "${target_platform}" != "linux-"* ]] && [[ "${target_platform}" != "osx-"* ]]; then
-#    rm testsuite/tests/unicode/$'\u898b'.ml
-#  fi
-#
-#  make ocamltest -j ${CPU_COUNT}
-#  make tests || true
-#fi
+if [[ ${CONDA_BUILD_CROSS_COMPILATION:-"0"} == "0" ]]; then
+  if [ "$(uname)" = "Darwin" ]; then
+    # Tests failing on macOS. Seems to be a known issue.
+    rm testsuite/tests/lib-str/t01.ml
+    rm testsuite/tests/lib-threads/beat.ml
+  fi
 
-mkdir -p ${PREFIX}/lib
+  if [[ "${target_platform}" != "linux-"* ]] && [[ "${target_platform}" != "osx-"* ]]; then
+    rm testsuite/tests/unicode/$'\u898b'.ml
+  fi
+
+  make ocamltest -j ${CPU_COUNT}
+  make tests
+fi
+
+mkdir -p ${OCAML_PREFIX}/lib
 make install
 
-for bin in $PREFIX/bin/*
+for bin in ${OCAML_PREFIX}/bin/*
 do
   if file "$bin" | grep -q "script executable"; then
     sed -i "s#exec '\([^']*\)'#exec \1#" "$bin"
-    sed -i "s#exec $PREFIX/bin#exec \$(dirname \"\$0\")#" "$bin"
+    sed -i "s#exec ${OCAML_PREFIX}/bin#exec \$(dirname \"\$0\")#" "$bin"
   fi
 done
 
 for CHANGE in "activate" "deactivate"
 do
   mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-  cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+  cp "${RECIPE_DIR}/scripts/${CHANGE}.${SH_EXT}" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.${SH_EXT}"
 done
