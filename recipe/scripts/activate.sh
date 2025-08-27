23b524eaf8e11b6e81566a275c954681781a6f5b
Memento "RC" Mori <claude.rc@gmail.com>
2025-08-27 17:58:27 -0500

    Update OCaml path in activate.sh script

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -4,4 +4,4 @@ export _OCAML_OCAML_PREFIX_CONDA_BACKUP=${OCAML_PREFIX:-}
 export OCAMLLIB=$CONDA_PREFIX/lib/ocaml
 export OCAML_PREFIX=$CONDA_PREFIX
 
-sed -i "2s#/opt/anaconda1anaconda2anaconda3#${CONDA_PREFIX}#" ${CONDA_PREFIX}/lib/ocaml/runtime-launch-info
+sed -i "2s#/opt/ocaml1ocaml2ocaml3#${CONDA_PREFIX}#" ${CONDA_PREFIX}/lib/ocaml/runtime-launch-info
