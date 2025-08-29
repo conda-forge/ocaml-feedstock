323ef233db5632787646eec842c10bf733b7e6c7
mementorc <claude.rc@gmail.com>
2025-08-29 10:34:54 -0500

    (fix) non-unix env vars

--- recipe/scripts/activate.bat
+++ recipe/scripts/activate.bat
@@ -1,11 +1,11 @@
-@if defined OCAML_OCAMLLIB (
-     @set "_OCAML_OCAMLLIB_BACKUP=%OCAML_OCAMLLIB%"
-)
-@set "OCAML_OCAMLLIB=%CONDA_PREFIX%\Library\lib\ocaml"
-
 @if defined OCAML_PREFIX (
      @set "_OCAML_PREFIX_BACKUP=%OCAML_PREFIX%"
 )
 @set "OCAML_PREFIX=%CONDA_PREFIX%\Library"
 
-set "PATH=%CONDA_PREFIX%\Library\lib\ocaml\stublibs;%PATH%"
+@if defined OCAMLLIB (
+     @set "_OCAML_OCAMLLIB_BACKUP=%OCAMLLIB%"
+)
+@set "OCAMLLIB=%OCAML_PREFIX%\lib\ocaml"
+
+set "PATH=%OCAMLLIB%\stublibs;%PATH%"
